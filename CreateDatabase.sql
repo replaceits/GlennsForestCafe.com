75ea79a95e651bd41bca324452b53361e7a4e381
replaceits <sharpcut2231@gmail.com>
2017-03-03 05:51:25 -0500

    Add login page

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -12,3 +12,9 @@ ALTER TABLE food_item_types ADD CONSTRAINT FK_food_item_type FOREIGN KEY ( food_
 CREATE TABLE food_image( food_image_id int(5) NOT NULL AUTO_INCREMENT, food_item_id int(5) NOT NULL, food_image_url nvarchar(5000), PRIMARY KEY( food_image_id, food_item_id ) );
 ALTER TABLE food_image ADD CONSTRAINT FK_food_item_2 FOREIGN KEY ( food_item_id ) REFERENCES food_item( food_item_id ) ON DELETE CASCADE ON UPDATE CASCADE;
 
+CREATE TABLE users( user_id int(5) NOT NULL AUTO_INCREMENT, user_first_name nvarchar(255) NOT NULL, user_last_name nvarchar(255),user_email nvarchar(255) NOT NULL, user_password_hash nvarchar(255) NOT NULL, PRIMARY KEY(user_id));
+
+CREATE TABLE user_admins( user_admin_id int(5) NOT NULL AUTO_INCREMENT, user_id int(5) NOT NULL, PRIMARY KEY(user_admin_id, user_id) );
+ALTER TABLE user_admins ADD CONSTRAINT FK_user_id_admin FOREIGN KEY ( user_id ) REFERENCES users( user_id ) ON DELETE CASCADE ON UPDATE CASCADE;
+
+
--- php/layout/navbar.php
+++ php/layout/navbar.php
@@ -11,29 +11,34 @@
         <a class="navbar-brand" href="">Glenn's Forest Cafe</a>
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav navbar-center">
-            <li><a href="about/"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;About<span class="sr-only">&nbsp;(current)</span></a></li>
-            <li><a href="location/"><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>&nbsp;Location</a></li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-cutlery" aria-hidden="true"></span>&nbsp;Menu&nbsp;<span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="menu/">All</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="menu/?sandwiches=1">Sandwiches</a></li>
-                    <li><a href="menu/?wraps=1">Wraps</a></li>
-                    <li><a href="menu/?salads=1">Salads</a></li>
-                    <li><a href="menu/?sides=1">Sides</a></li>
-                    <li><a href="menu/?drinks=1">Drinks</a></li> 
-                    <li role="separator" class="divider"></li>
-                    <li><a href="menu/?specials=1">Specials</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="menu/download/">Download menu&nbsp;&nbsp;<span class="glyphicon glyphicon-download" aria-hidden="true"></span></a></li>
+            <ul class="nav navbar-nav navbar-center">
+                <li><a href="about/"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;About<span class="sr-only">&nbsp;(current)</span></a></li>
+                <li><a href="location/"><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>&nbsp;Location</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-cutlery" aria-hidden="true"></span>&nbsp;Menu&nbsp;<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="menu/">All</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="menu/?sandwiches=1">Sandwiches</a></li>
+                        <li><a href="menu/?wraps=1">Wraps</a></li>
+                        <li><a href="menu/?salads=1">Salads</a></li>
+                        <li><a href="menu/?sides=1">Sides</a></li>
+                        <li><a href="menu/?drinks=1">Drinks</a></li> 
+                        <li role="separator" class="divider"></li>
+                        <li><a href="menu/?specials=1">Specials</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="menu/download/">Download menu&nbsp;&nbsp;<span class="glyphicon glyphicon-download" aria-hidden="true"></span></a></li>
+                    </ul>
+                </li>
+            </ul>
+            <div class="navbar-right">
+                <ul class="nav navbar-nav">
+                    <li><a href="login/"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;Login</a></li>
                 </ul>
-            </li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-            <li><a href="cart/"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>&nbsp;Cart&nbsp;<span class="cart-header-items badge">0</span></a></li>
-        </ul>
+                <ul class="nav navbar-nav">
+                    <li><a href="cart/"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>&nbsp;Cart&nbsp;<span class="cart-header-items badge">0</span></a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </header>
